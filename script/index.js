9c5f06d6f14c02636bf69aa8661855c5482efd37
NEBIST-repositorio <repositorio.neb@gmail.com>
2022-03-02 23:10:28 +0000

    6th commit

--- script/index.js
+++ script/index.js
@@ -1,7 +1,30 @@
-document.onload = function() {
+let jsData; // LEBIO repo information
+const URL = "https://nebist-repositorio.github.io/BioDrive/script/data.json"
 
+
+
+window.onload = function() {
+    readJSON();
+};
+
+
+function readJSON() {
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', URL, true);
+    xhr.responseType = 'json';
+
+    xhr.onload = function() {
+        jsData = xhr.response;
+        console.log(jsData);
+    };
+    xhr.send();
 }
 
+function addToTable(value) {
+    console.log(value);
+    console.log(jsData[value]);
+}
 
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
