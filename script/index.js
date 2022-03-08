e3fb8cc8bedcfe383bb32afa0688e23f5edcbaac
NEBIST-repositorio <repositorio.neb@gmail.com>
2022-03-08 03:34:59 +0000

    BETA VERSION optimised
    Refreshing works, optimised js, a few css changes bigger font padding etc

--- script/index.js
+++ script/index.js
@@ -3,57 +3,55 @@ const URL = "https://nebist-repositorio.github.io/BioDrive/script/data.json"
 
 
 
-window.onload = function() {
-    readJSON();
+window.onload = () => {
+    displayPage();
+}
 
-    // Change to right year
 
-    division = window.location.href.split("/")
-    page = division[division.length - 1]
-    if (page !== "arquivo.html" && page !== "indexprivate.html" && page !== "encrypted.html" && page !== "index.html") {
-        let queryString = window.location.search;
-        let urlParams = new URLSearchParams(queryString);
-        let year = urlParams.get('ano');
-        changeYear(year);
+async function displayPage() {
+    jsData = await (await fetch(URL)).json();
+    let queryString = window.location.search;
+    let urlParams = new URLSearchParams(queryString);
+    let path = urlParams.get('local');
+
+    if (path !== null) {
+        addGlobal(path);
     }
+}
 
+function setLocal(local) {
+    window.location.href = `../html/ano.html?local=${local}`;
 }
 
 
-function readJSON() {
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', URL, true);
-    xhr.responseType = 'json';
-
-    xhr.onload = function() {
-        jsData = xhr.response;
-    };
-    xhr.send();
-}
-
-
-
-function goToYear(year) {
-    window.location.href = "../html/ano.html?ano=" + year;
-}
-
-function appendHeader(value, func) {
+function createHeader(path, path_len) {
     const header = document.getElementById("header_ano");
-    element = header.getElementsByClassName(value);
+    let jsData_copy = jsData;
+    let sum_path = "";
 
-    if (element.length == 0) {
-        header.innerHTML += `<i class = "fa-solid fa-angle-right fa-xs" ></i><a class="` + value + `" onclick=` + func + `>` + value + `</a>`;
-    } else {
-        // Element already created; we have to remove all childs
-        var child = header.lastElementChild;
+    for (var i = 0; i < path_len - 1; i++) {
+        var value = path[i];
+        sum_path += value;
+        jsData_copy = jsData_copy[value];
 
-        while (child !== element[0]) {
-            header.removeChild(child);
-            child = header.lastElementChild;
+        if (value === "folder") {
+            value = jsData_copy.name;
         }
+        header.innerHTML += `<a onclick="setLocal('${sum_path}')">${value}</a><i class="fa-solid fa-angle-right fa-xs"></i>`;
+        sum_path += `/`;
     }
+    value = path[i];
+    sum_path += value;
+    jsData_copy = jsData_copy[value];
+
+    if (value === "folder") {
+        value = jsData_copy.name;
+    }
+    header.innerHTML += `<a onclick="setLocal('${sum_path}')">${value}</a>`;
+
+    return jsData_copy;
 }
+
 
 // removes all nodes from table body
 function removeContent() {
@@ -68,41 +66,11 @@ function removeContent() {
 }
 
 
-function changeYear(year) {
-    // Sets header 
-    document.getElementById("header_ano").innerHTML =
-        `<a onclick="changeYear('` + year + `')">` + year + `</a>`;
-    addSemesters(year);
-}
-
-
-// Adds Semesters to Year on table body 
-function addSemesters(year) {
-    removeContent();
-
-    document.getElementById("cont_table").innerHTML +=
-        `<tr><td><a onclick="addGlobal('` + year + `/1º Semestre')"> 1º Semestre </a></td></tr> 
-         <tr><td><a onclick="addGlobal('` + year + `/2º Semestre')"> 2º Semestre </a></td></tr>`;
-}
-
-
 function addGlobal(path) {
-    removeContent();
     const path_split = path.split('/');
     const path_len = path_split.length;
-    const func = "\"addGlobal('" + path + "')\"";
 
-
-    let jsData_copy = jsData;
-    for (let i = 0; i < path_len; i++) {
-        jsData_copy = jsData_copy[path_split[i]];
-    }
-
-    let location = path_split[path_len - 1];
-    if (location === "folder")
-        location = jsData_copy.name
-
-    appendHeader(location, func);
+    let jsData_copy = createHeader(path_split, path_len);
 
     table_body = document.getElementById("cont_table");
     for (let i in jsData_copy) {
@@ -115,7 +83,7 @@ function addGlobal(path) {
             }
             if (i !== "name") {
                 new_path = path + "/" + i;
-                table_body.innerHTML += `<tr><td colspan="2"><a onclick="addGlobal('` + new_path + `')">` + suffix + `</a></td></tr>`;
+                table_body.innerHTML += `<tr><td colspan="2"><a onclick="setLocal('${new_path}')">${suffix}</a></td></tr>`;
             }
         }
     }
@@ -127,11 +95,12 @@ function displayFiles(data) {
     for (let file in data) {
         table_body.innerHTML += `
     <tr>
-        <td><a href="https://drive.google.com/file/d/` + data[file].id + `/view "> ` + data[file].name + `</a></td>
-        <td><a href="https://drive.google.com/uc?export=download&id=` + data[file].id + ` "><i class="fa-solid fa-download fa-lg "></i></td>
+        <td><a href="https://drive.google.com/file/d/${data[file].id}/view">${data[file].name}</a></td>
+        <td><a href="https://drive.google.com/uc?export=download&id=${data[file].id}"><i class="fa-solid fa-download fa-lg "></i></td>
     </tr>`
     }
 }
+
 
 
 
